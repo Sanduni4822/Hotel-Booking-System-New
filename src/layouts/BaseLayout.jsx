8c9ee86aa7e14a2b15994163749be2dd0cfeec7a
sanduni4822 <sanduniwathsala0123@gmail.com>
2024-06-10 18:26:36 -0700

    Navigationbar components the BaseLayout.jsx

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BaseLayout = () => {
   return (
-    < div>BaseLayout</div>
-  )
-}
+    <nav className="navbar">
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/Rooms">Rooms</Link></li>
+        <li><Link to="/Activities">Activities</Link></li>
+        <li><Link to="/Services">Services</Link></li>
+        <li><Link to="/Facilities">Facilities</Link></li>
+        <li><Link to="/Contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
 
 export default BaseLayout;
