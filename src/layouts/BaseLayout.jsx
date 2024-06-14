7e4b5c5e9d16ead4b48925899294e47a111147e4
sanduni4822 <sanduniwathsala0123@gmail.com>
2024-06-14 04:07:52 -0700

    Upated navbar elements

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -3,57 +3,59 @@ import { Link } from 'react-router-dom';
 
 const BaseLayout = () => {
   return (
-    <nav className="bg-black p-4 flex justify-between items-center">
-      <ul className="flex space-x-4">
-        <li>
-          <Link to="/" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/rooms" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
-            Rooms
-          </Link>
-        </li>
-        <li>
-          <Link to="/activities" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
-            Activities
-          </Link>
-        </li>
-        <li>
-          <Link to="/services" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
-            Services
-          </Link>
-        </li>
-        <li>
-          <Link to="/facilities" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
-            Facilities
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
-            Contact
-          </Link>
-        </li>
-      </ul>
-      <ul className="flex space-x-4">
-        <li>
-          <Link to="/login" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
-            Login/Join
-          </Link>
-        </li>
-        <li>
-          <Link to="/signup" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
-            Sign Up
-          </Link>
-        </li>
-        <li>
-          <Link to="/book" className="text-black bg-white hover:bg-gray-300 px-3 py-2 rounded">
-            Book Now!
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="flex flex-col min-h-screen">
+      <nav className="bg-black p-4 flex justify-between items-center">
+        <ul className="flex space-x-4">
+          <li>
+            <Link to="/" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/rooms" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
+              Rooms
+            </Link>
+          </li>
+          <li>
+            <Link to="/activities" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
+              Activities
+            </Link>
+          </li>
+          <li>
+            <Link to="/offers" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
+              Offer
+            </Link>
+          </li>
+          <li>
+            <Link to="/facilities" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
+              Facilities
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <ul className="flex space-x-4">
+          <li>
+            <Link to="/login" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
+              Login/Join
+            </Link>
+          </li>
+          <li>
+            <Link to="/signup" className="text-white hover:bg-gray-700 px-3 py-2 rounded">
+              Sign Up
+            </Link>
+          </li>
+          <li>
+            <Link to="/book" className="text-black bg-white hover:bg-gray-300 px-3 py-2 rounded">
+              Book Now!
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
