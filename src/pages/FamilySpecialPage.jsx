127f178008a49283dc182882bb3abfaa9e8463b4
sanduni4822 <sanduniwathsala0123@gmail.com>
2024-09-16 08:33:59 +0530

    Created the FamilySpecialRoomRate component & called it in the FamilySpecialPage

--- src/pages/FamilySpecialPage.jsx
+++ src/pages/FamilySpecialPage.jsx
@@ -26,6 +26,8 @@ import maxguests from '../assets/Icons/maxguests.png';
 import bedtype from '../assets/Icons/bedtype.png'; 
 import area from '../assets/Icons/area.png'; 
 
+import FamilySpecialRoomRate from '../components/RoomRateComponent/FamilySpecialRoomRate';
+
 const FamilySpecialPage = () => {
   const settings = {
     dots: true,
@@ -127,7 +129,9 @@ const FamilySpecialPage = () => {
         {/* Right Column (Leave empty or add more content later) */}
         <div className="space-y-6">
           {/* Additional content can go here if needed */}
+          <FamilySpecialRoomRate />
         </div>
+        
 
       </div>
     </div>
