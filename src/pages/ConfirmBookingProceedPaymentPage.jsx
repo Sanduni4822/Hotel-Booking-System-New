aa73d85f5bdab28a323374cac09960f0893272b7
sanduni4822 <sanduniwathsala0123@gmail.com>
2024-09-19 17:27:30 +0530

    Created the rounded button for submit button

--- src/pages/ConfirmBookingProceedPaymentPage.jsx
+++ src/pages/ConfirmBookingProceedPaymentPage.jsx
@@ -249,7 +249,7 @@ const ConfirmBookingProceedPaymentPage = () => {
             {/* Submit Button */}
             <button
               type="submit"
-              className="w-full bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600 focus:outline-none focus:ring"
+              className="bg-red-500 text-white py-2 px-4 rounded-full hover:bg-red-600 focus:outline-none focus:ring"
             >
               Submit
             </button>
