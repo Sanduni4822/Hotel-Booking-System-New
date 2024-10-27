be1ea223139586ae2bdcb6936439d294401bc465
sanduni4822 <sanduniwathsala0123@gmail.com>
2024-10-27 14:21:28 +0530

    Setup RoomsDetailsPage basic structure with title and container

--- src/pages/Receptionist/RoomsDetailsPage.jsx
+++ src/pages/Receptionist/RoomsDetailsPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const RoomsDetailsPage = () => {
-    return (
-      < div>bus</div>
-    )
-  }
-  
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h2 className="text-3xl font-bold mb-4 text-center">Existing Rooms</h2>
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        {/* Filter and Table Container */}
+      </div>
+    </div>
+  );
+};
+
 export default RoomsDetailsPage;
